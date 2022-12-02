dd8cdc73cbf8fa5ed8e499ac674dcacf659ba55e
Elia Maggioni <elia.maggioni@gmail.com>
2022-12-02 11:03:35 +0100

    prettier on save

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './app/app'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './app/app';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
 	<React.StrictMode>
 		<App />
 	</React.StrictMode>
-)
+);
